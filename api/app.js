9ab30eb34ab26547372794c868c3c1cc0b1bfad9
Zachary Baca <bacazach@icloud.com>
2023-04-11 20:45:47 -0500

    updated url for cors to railway link

--- api/app.js
+++ api/app.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:3000",
+    origin: "school-course-catalog-front-end-production.up.railway.app",
   })
 );
 
